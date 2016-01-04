5e2937a0c1af0f1ed11c405f183b48dcac8779c2
kf4btg <Kf4btg@users.noreply.github.com>
2016-01-03 20:00:46 -0500

    added Notifier object to help syncronization

--- skymodman/utils.py
+++ skymodman/utils.py
@@ -22,4 +22,29 @@ def withlogger(cls):
     return cls
 
 
+class Notifier:
+    """Creates a callable object that waits to be called `count` number of times, then executes the given `callback` with the given args and kwargs"""
 
+    def __init__(self, count, callback, *cb_args, **cb_kwargs):
+        self.count = count
+        self.callback = callback
+        self.cbargs = cb_args
+        self.cbkwargs = cb_kwargs
+
+        self.timer = self.notify_wait(count)
+
+    def __call__(self, who=None):
+        """:param who: optional string to provide context about the caller to the user; only used for logging purposes"""
+        try:
+            # print("--call from {}".format(who))
+            next(self.timer)
+        except StopIteration:
+            # print("--done--")
+            self.callback(*self.cbargs, **self.cbkwargs)
+
+    @staticmethod
+    def notify_wait(count):
+        calls = 1
+        while calls < count:
+            yield
+            calls+=1
