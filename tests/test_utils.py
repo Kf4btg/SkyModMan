cc4115d7136a70d35b60b69769138be83108a9c6
kf4btg <Kf4btg@users.noreply.github.com>
2016-01-17 03:35:23 -0500

    testing out testing

---
+++ tests/test_utils.py
@@ -0,0 +1,44 @@
+from skymodman.utils import allcombos, checkPath, reduceall
+import pytest
+
+testpaths = [("", False), (".", True), ("/", True),
+             ("/home/../home/../", True), ("~", False),
+             ("~/Desktop", False), ("/nadadontexist", False),
+             ("/..", True)]
+
+testpaths2 = [("", False), (".", True), ("~", True),
+              ("~/../", True), ("~/Desktop", True),
+              ("~/~", False), ("~/notathingnotexistinghere", False),
+              ("~/../~", False)]
+
+idfn = lambda v: str(v)
+
+@pytest.mark.parametrize("path, expect", testpaths, ids=idfn)
+def test_checkpath_noexpand(path, expect):
+    assert bool(checkPath(path)) == expect
+
+
+@pytest.mark.parametrize("path,expect", testpaths2, ids=idfn)
+def test_checkpath_expand(path, expect):
+    assert bool(checkPath(path, True)) == expect
+
+
+def test_allcombos():
+
+    it = ['a','b','c']
+    exp_res = [('a',), ('b',), ('c',),
+               ('a','b'), ('a','c'),
+               ('b','c'), ('a','b','c')]
+
+    assert [t for t in allcombos(it)] == exp_res
+
+    assert [t for t in allcombos('abc')] == exp_res
+
+def test_reduceall():
+    import operator
+
+    op = operator.mul
+    lol = [(1, 2), (2, 3, 4), (3, 4, 5, 6)]
+    expres = [2, 24, 360]
+
+    assert list(reduceall(op, lol)) == expres
