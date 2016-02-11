0ed574e53e0e7a9777f3f1656c307e7e915d4d81
kf4btg <Kf4btg@users.noreply.github.com>
2016-02-11 01:58:35 -0500

    tweaks to printattrs

--- skymodman/utils/__init__.py
+++ skymodman/utils/__init__.py
@@ -74,11 +74,13 @@ def change_dir(dir_):
 
 
 
-def printattrs(obj, name=None):
+def printattrs(obj, name=None, dunder=True, sunder=True):
     """
     For debugging. Prints the name and value of any entry in the object's dir(). Works for classes with or without a __dict__
     :param obj:
     :param name: Just used to label the object in output
+    :param dunder: Include double-underscore __names__ in output
+    :param sunder: Include single-underscore _names in output
     :return:
     """
     from pprint import pprint
@@ -89,8 +91,15 @@ def printattrs(obj, name=None):
         print("attributes:")
 
     for a in dir(obj):
+        if not dunder and a.endswith("__"): continue
+        if not sunder and a.startswith("_"): continue
         print("    ",end="")
-        attr=getattr(obj,a)
+
+        try:
+            attr=getattr(obj,a)
+        except AttributeError:
+            # I have no idea how this happens...
+            continue
         if isinstance(attr,(Mapping,Sequence)) and not isinstance(attr,(str,bytes)):
             print(a,end=": ")
             pprint(attr, indent=4)
