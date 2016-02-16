afc59dd2c4496cfcdb6a7ea48dba464b424e43e0
kf4btg <Kf4btg@users.noreply.github.com>
2016-02-16 00:34:47 -0500

    add debug utility module

--- skymodman/utils/__init__.py
+++ skymodman/utils/__init__.py
@@ -32,37 +32,7 @@ def withlogger(cls):
 
     return cls
 
-def printattrs(obj, name=None, dunder=True, sunder=True):
-    """
-    For debugging. Prints the name and value of any entry in the object's dir(). Works for classes with or without a __dict__
-    :param obj:
-    :param name: Just used to label the object in output
-    :param dunder: Include double-underscore __names__ in output
-    :param sunder: Include single-underscore _names in output
-    :return:
-    """
-    from pprint import pprint
-    from collections import Mapping, Sequence
-    if name:
-        print(name+":")
-    else:
-        print("attributes:")
 
-    for a in dir(obj):
-        if not dunder and a.endswith("__"): continue
-        if not sunder and a.startswith("_"): continue
-        print("    ",end="")
-
-        try:
-            attr=getattr(obj,a)
-        except AttributeError:
-            # I have no idea how this happens...
-            continue
-        if isinstance(attr,(Mapping,Sequence)) and not isinstance(attr,(str,bytes)):
-            print(a,end=": ")
-            pprint(attr, indent=4)
-        else:
-            print(a, attr, sep=": ")
 
 # FROM:
 # http://stackoverflow.com/questions/24601722/how-can-i-use-functools-singledispatch-with-instance-methods
